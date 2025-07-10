40a6bf05755ec55ac0950dcaac1795ca2733a381
Rean Tsai <aobc55625@gmail.com>
2025-07-10 22:11:09 +0800

    feat: add Google OAuth user table for third-party authentication

--- models/schema.js
+++ models/schema.js
@@ -3,3 +3,4 @@ export { roleEnum } from './enums/roleEnum.js';
 export { statusEnum } from './enums/statusEnum.js';
 export { usersTable } from './users/usersTable.js';
 export { emailUsersTable } from './users/emailUsersTable.js';
+export { googleUsersTable } from './users/googleUsersTable.js';
