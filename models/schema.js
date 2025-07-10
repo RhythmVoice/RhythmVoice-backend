638c6848209214844692b9bb34466c5cc88a2b87
Rean Tsai <aobc55625@gmail.com>
2025-07-10 22:16:47 +0800

    feat: add LINE OAuth user table to complete multi-provider authentication

--- models/schema.js
+++ models/schema.js
@@ -4,3 +4,4 @@ export { statusEnum } from './enums/statusEnum.js';
 export { usersTable } from './users/usersTable.js';
 export { emailUsersTable } from './users/emailUsersTable.js';
 export { googleUsersTable } from './users/googleUsersTable.js';
+export { lineUsersTable } from './users/lineUsersTable.js';
