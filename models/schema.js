6f2b059f28b698aca1f24b6ab8f8e4792233c636
Rean Tsai <aobc55625@gmail.com>
2025-07-10 22:06:08 +0800

    feat: create users table schema with authentication and role management

--- models/schema.js
+++ models/schema.js
@@ -1,3 +1,4 @@
 export { providerTypeEnum } from './enums/providerTypeEnum.js';
 export { roleEnum } from './enums/roleEnum.js';
 export { statusEnum } from './enums/statusEnum.js';
+export { usersTable } from './users/usersTable.js';
